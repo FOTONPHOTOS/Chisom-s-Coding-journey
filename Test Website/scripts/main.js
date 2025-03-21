fd9f8e15374ecf407dce322229d39cb3e9e05a16
Your Name <aniebochisom@gmail.com>
2025-03-21 12:38:36 +0100

    Updated code

--- Test Website/scripts/main.js
+++ Test Website/scripts/main.js
@@ -1,4 +1,6 @@
 const myHeading = document.querySelector("h1");
-myHeading.textContent = "hi";
+myHeading.textContent = "Unraveling the Past, One Story at a Time";
 
 console.log("Hello from script.js!");
+
+alert("hello");
