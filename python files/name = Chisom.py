ac03592cfb33f5621abdbea9af69a7e341da3851
web3chisom <118540177+FOTONPHOTOS@users.noreply.github.com>
2025-03-21 12:08:18 +0100

    Delete python files directory

--- python files/name = Chisom.py
+++
@@ -1,6 +0,0 @@
-name_of_project = "Chisom Learning"
-print(len(name_of_project))
-print(name_of_project.upper())
-print(name_of_project.lower())
-print(name_of_project.find("C"))
-print(name_of_project.replace("Chisom", "Lucky"))
